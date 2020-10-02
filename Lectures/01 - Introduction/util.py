bc038645fb8a32e18a20644e85978eab27ccea21
Marina Langlois <marinalanglois@Marinas-Air.san.rr.com>
2020-10-02 15:14:21 -0700

    Lecture 1: Introduction

---
+++ Lectures/01 - Introduction/util.py
@@ -0,0 +1,34 @@
+import zipfile
+import requests
+import os
+
+def safe_download(url):
+    '''
+    download data file at url to directory data.
+    '''
+    
+    fname = os.path.join('data', os.path.basename(url))
+
+    # create data dir if doesn't exist
+    if not os.path.exists('data'):
+        os.mkdir('data')
+
+    # only download if the file exists
+    if not os.path.exists(fname):
+        resp = requests.get(url)
+        resp.raise_for_status()
+
+        with open(fname, 'wb') as f:
+            f.write(resp.content)
+
+    # if the file is zipped, unzip it
+    dirname, ext = os.path.splitext(fname)
+    if ext == '.zip':
+        with zipfile.ZipFile(fname, 'r') as zip_ref:
+            zip_ref.extractall(dirname)
+
+        fname = dirname
+
+    return fname
+
+
