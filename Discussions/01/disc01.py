4d0b7b42bb219eb9167542a23632864c36935610
ShubhamKaushal15 <47340042+ShubhamKaushal15@users.noreply.github.com>
2020-10-07 12:30:21 -0700

    Added Discussion 01 Files

---
+++ Discussions/01/disc01.py
@@ -0,0 +1,39 @@
+
+import numpy as np
+import os
+
+def data2array(filepath):
+    """
+    data2array takes in the filepath of a 
+    data file like `restaurant.csv` in 
+    data directory, and returns a 1d array
+    of data.
+
+    :Example:
+    >>> fp = os.path.join('data', 'restaurant.csv')
+    >>> arr = data2array(fp)
+    >>> isinstance(arr, np.ndarray)
+    True
+    >>> arr.dtype == np.dtype('float64')
+    True
+    >>> arr.shape[0]
+    100000
+    """
+
+    return ...
+
+
+def ends_in_9(arr):
+    """
+    ends_in_9 takes in an array of dollar amounts 
+    and returns the proprtion of values that end 
+    in 9 in the hundredths place.
+
+    :Example:
+    >>> arr = np.array([23.04, 45.00, 0.50, 0.09])
+    >>> out = ends_in_9(arr)
+    >>> 0 <= out <= 1
+    True
+    """
+
+    return ...
